1617bcb29f92755c2ca46dd2390041abe3a30d61
Kenneth A. Bettum Gjermstad <kegj002@student.kristiania.no>
2024-11-12 18:56:53 +0100

    Update clock.test.js

--- code/src/clock.test.js
+++ code/src/clock.test.js
@@ -9,6 +9,6 @@ test('getTimeString formats time string correctly', () => {
 test('getSecondsLeftOfYear returns the correct amount of seconds', () => {
     const now = new Date("2022-12-31T23:00:00.000Z");
     const timeString = getSecondsLeftOfYear(now);
-    expect(timeString).toBe(3500);
+    expect(timeString).toBe(3600);
 });
 
